71037ca055e34dd7fa89632b5aebb22a7aff4a85
Mike Cheng <mfycheng@uwaterloo.ca>
2015-07-02 22:27:50 -0400

    Basic structure that projects should have at minimum.
    Includes stub files, because git does not include
    empty directories.

---
+++ include/skrillex/stub.hpp
@@ -0,0 +1,11 @@
+#ifndef stub_hpp
+#define stub_hpp
+
+namespace skrillex {
+    class Stub {
+    public:
+        bool doIHaveAnArm() const;
+    };
+}
+
+#endif
---
+++ src/stub.cpp
@@ -0,0 +1,7 @@
+#include "skrillex/stub.hpp"
+
+namespace skrillex {
+    bool Stub::doIHaveAnArm() const {
+        return false;
+    }
+}
