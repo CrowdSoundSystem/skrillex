73c4cc5a69e30929184c5b2bc5d3757bdee9bc05
Mike Cheng <michael.cheng@kik.com>
2015-07-08 18:49:46 -0400

    Implement initial status

--- include/skrillex/status.hpp
+++ include/skrillex/status.hpp
@@ -10,8 +10,56 @@
 #ifndef skrillex_status_hpp
 #define skrillex_status_hpp
 
+#include <string>
+
 namespace skrillex {
-    class Status;
+    class Status {
+        enum class Code {
+            OK = 0,
+            NotFound = 1,
+            Error = 2
+        };
+
+        Code        code_;
+        std::string message_;
+
+    private:
+        Status(Code code, std::string message)
+        : code_(code)
+        , message_(std::move(message))
+        {
+        }
+
+    public:
+        Status()
+        : code_(Code::OK)
+        {
+        }
+
+        static Status OK() {
+            return Status();
+        }
+
+        static Status NotFound(std::string message) {
+            return Status(Code::NotFound, message);
+        }
+        static Status Error(std::string message) {
+            return Status(Code::Error, message);
+        }
+
+        bool ok() const       { return code_ == Code::OK; }
+        bool notFound() const { return code_ == Code::NotFound; }
+        bool error() const    { return code_ == Code::Error; }
+
+        std::string string() const {
+            switch (code_) {
+                case Code::OK:       return "Status: OK";
+                case Code::NotFound: return "Status: NotFound - " + message_;
+                case Code::Error:    return "Status: Error - " + message_;
+                default:             return message_;
+            }
+        }
+    };
 }
 
 #endif
