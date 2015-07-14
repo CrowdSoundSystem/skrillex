d14ef98818fff6015abc8a7291c7f9ab43241108
Mike Cheng <mfycheng@uwaterloo.ca>
2015-07-14 04:34:09 -0400

    Sqlite3 store interface

---
+++ src/store/sqlite3_store.hpp
@@ -0,0 +1,54 @@
+#ifndef skrillex_sqlite3store_hpp
+#define skrillex_sqlite3store_hpp
+
+#include "skrillex/dbo.hpp"
+#include "skrillex/options.hpp"
+#include "skrillex/result_set.hpp"
+#include "skrillex/status.hpp"
+#include "store/store.hpp"
+
+#include "sqlite3/sqlite3.h"
+
+namespace skrillex {
+namespace internal {
+    class Sqlite3Store : public Store {
+    public:
+        Sqlite3Store();
+        Sqlite3Store(int session_id);
+        Sqlite3Store(const Sqlite3Store& other) = delete;
+        Sqlite3Store(Sqlite3Store&& other)      = delete;
+        ~Sqlite3Store();
+
+        Status getSongs(ResultSet<Song>& set, ReadOptions options);
+        Status getArtists(ResultSet<Artist>& set, ReadOptions options);
+        Status getGenres(ResultSet<Genre>& set, ReadOptions options);
+
+        Status getPlayHistory(ResultSet<Song>& set, ReadOptions options);
+
+        Status getQueue(ResultSet<Song>& set);
+        Status queueSong(int songId);
+        Status songFinished();
+
+        Status addSong(Song& song);
+        Status addArtist(Artist& artist);
+        Status addGenre(Genre& genre);
+
+        Status countSong(Song& song, WriteOptions options);
+        Status countArtist(Artist& artist, WriteOptions options);
+        Status countGenre(Genre& genre, WriteOptions options);
+
+        Status voteSong(Song& song, int amount, WriteOptions options);
+        Status voteArtist(Artist& artist, int amount, WriteOptions options);
+        Status voteGenre(Genre& genre, int amount, WriteOptions options);
+
+        Status createSession();
+        Status createSession(int& result);
+
+        Status getSession(int& result);
+        Status getSessionCount(int& result);
+    };
+}
+}
+
+#endif
+
