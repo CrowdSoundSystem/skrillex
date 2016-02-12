60ade65b887e3892f9fc8dcb1e25f47952c9ab85
Mike Cheng <mfycheng@uwaterloo.ca>
2016-02-12 02:24:26 -0500

    Add some mapper documentation

--- include/skrillex/mapper.hpp
+++ include/skrillex/mapper.hpp
@@ -26,7 +26,22 @@ namespace skrillex {
         Mapper(std::shared_ptr<DB> db);
         ~Mapper();
 
+        // Map takes in three inputs, the song name, artist name, and genre name.
+        //
+        // It then attempts to map each value to their respective DBO object. If
+        // a mapping cannot be found, a new DBO is created for the missing value.
+        //
+        // If any of the input fields are empty (with the exception of SongName),
+        // they are ignored.
+        //
+        // Result the mapped Song object.
+        //
+        // Note: This function may modify the underlying store.
         Status map(Song& result, std::string song, std::string artist, std::string genre);
+
+        // Lookup attempts to lookup a corresponding song for a given
+        // <song name, artist name> combination. If a song cannot be
+        // found, Status::NotFound() is returned.
         Status lookup(Song& result, std::string song, std::string artist);
     };
 }
