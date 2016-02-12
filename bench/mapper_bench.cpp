bb2f73cde3922dd4051238cdaa8aa31922abef7b
Mike Cheng <mfycheng@uwaterloo.ca>
2016-02-12 02:16:18 -0500

    Add some basic mapper benches

--- bench/mapper_bench.cpp
+++ bench/mapper_bench.cpp
@@ -1,7 +1,10 @@
 #include "util/time.hpp"
 
+#include <algorithm>
+#include <functional>
 #include <iostream>
 #include <memory>
+#include <random>
 #include <stdlib.h>
 
 #include "skrillex/skrillex.hpp"
@@ -10,6 +13,8 @@ using namespace std;
 using namespace skrillex;
 using namespace skrillex::internal;
 
+const string characters = "abcdefghijklmnopqrstuvwxyz1234567890";
+
 void checkStatus(Status status) {
     if (status != Status::OK()) {
         cerr << status.message() << endl;
@@ -17,17 +22,106 @@ void checkStatus(Status status) {
     }
 }
 
-int main() {
+string randomString(int length, function<char(void)> randChar) {
+    string s(length, 0);
+    generate_n(s.begin(), length, randChar);
+    return s;
+}
+
+void benchMapNew(function<char(void)> randChar) {
     DB* raw = 0;
     checkStatus(open(raw, "bench.db", Options::TestOptions()));
     shared_ptr<DB> db(raw);
     shared_ptr<Mapper> mapper(new Mapper(db));
 
-    Song song;
-    auto start = now();
-    Status s = mapper->map(song, "", "", " Genre");
-    auto end = now();
+    for (int i = 0; i < 1000; i++) {
+        Song song;
 
-    checkStatus(s);
-    cout << "Time: " << (end - start).count() << " ns" << endl;
+        string genreName = randomString(5, randChar);
+        string artistName = randomString(10, randChar);
+        string songName = randomString(10, randChar);
+
+        auto start = now();
+        checkStatus(mapper->map(song, songName, artistName, genreName));
+        auto end = now();
+
+        cout << (end - start).count() << endl;
+    }
+}
+
+void benchMapExisting() {
+    DB* raw = 0;
+    checkStatus(open(raw, "bench.db", Options::TestOptions()));
+    shared_ptr<DB> db(raw);
+    shared_ptr<Mapper> mapper(new Mapper(db));
+
+    string genreName = "12345";
+    string artistName = "thisis10le";
+    string songName = "also10lett";
+
+    for (int i = 0; i < 1000; i++) {
+        Song song;
+
+        auto start = now();
+        checkStatus(mapper->map(song, songName, artistName, genreName));
+        auto end = now();
+
+        cout << (end - start).count() << endl;
+    }
+}
+
+void benchLookupExisting() {
+    DB* raw = 0;
+    checkStatus(open(raw, "bench.db", Options::TestOptions()));
+    shared_ptr<DB> db(raw);
+    shared_ptr<Mapper> mapper(new Mapper(db));
+
+    string genreName = "12345";
+    string artistName = "thisis10le";
+    string songName = "also10lett";
+
+    Song song;
+    checkStatus(mapper->map(song, songName, artistName, genreName));
+
+    for (int i = 0; i < 1000; i++) {
+        Song song;
+        auto start = now();
+        checkStatus(mapper->lookup(song, songName, artistName));
+        auto end = now();
+
+        cout << (end - start).count() << endl;
+    }
+}
+
+void benchLookupNonExisting() {
+    DB* raw = 0;
+    checkStatus(open(raw, "bench.db", Options::TestOptions()));
+    shared_ptr<DB> db(raw);
+    shared_ptr<Mapper> mapper(new Mapper(db));
+
+    for (int i = 0; i < 1000; i++) {
+        Song song;
+        auto start = now();
+        Status s = mapper->lookup(song, "1234567890", "098765431");
+        if (!s.notFound()) {
+            cout << "Expected not found, but was found" << endl;
+            exit(1);
+        }
+        auto end = now();
+        cout << (end - start).count() << endl;
+    }
+}
+
+
+int main() {
+    default_random_engine rng(random_device{}());
+    uniform_int_distribution<> dist(0, characters.size() - 1);
+    auto randChar = [&dist, &rng]() { return characters[dist(rng)]; };
+
+    // For consistency, all tests use:
+    //     * 1000 iterations
+    //     * 10 character song names
+    //     * 10 character artist names
+    //     * 5 character genre names
+    benchLookupNonExisting();
 }
